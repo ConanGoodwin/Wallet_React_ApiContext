ecd38a32e6860e2243f90cb0f19d41bba1b1ba21
ConanGoodwin <conan_goodwin@hotmail.com>
2022-08-27 23:42:23 -0300

    disponibilizando os dados do gasto nos inputs para edição

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -38,7 +38,7 @@ export function errorConect({ message }) {
 export const editExpense = (state) => async (dispatch) => {
   try {
     dispatch({ type: IS_LOADING });
-    const data = await connectApi();
+    const data = await connectApi();// teste
 
     await dispatch({
       type: EDIT_EXPENSE,
